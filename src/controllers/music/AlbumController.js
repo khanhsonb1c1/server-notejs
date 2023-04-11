f9d6cd691c78ff7f514fcfb6d91b7488b1267d52
khanhsonb1c1 <khanhsonb1c1@gmail.com>
2023-04-12 00:08:01 +0700

    delete album

--- src/controllers/music/AlbumController.js
+++ src/controllers/music/AlbumController.js
@@ -2,7 +2,8 @@ const { Album } = require("../../models/music/AlbumModel");
 const fs = require("fs-extra");
 const { Singer } = require("../../models/music/SingerModel");
 
-const cloudinary = require("../../services/cloudinary")
+const cloudinary = require("../../services/cloudinary");
+const { Music } = require("../../models/music/MusicModel");
 
 const albumController = {
   create: async (req, res) => {
@@ -66,6 +67,39 @@ const albumController = {
     try {
       const album = await Album.findOne({ id: req.params.id });
       res.status(200).json(album);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const music = await Album.findById(req.params.id);
+      // $set: thay thế object
+      await music.updateOne({ $set: req.body });
+      res.status(200).json("Updated successfully !");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const music = Music.find({
+        album: req.params.id,
+      });
+      await music[0].updateOne({
+        $push: { album: "" },
+      });
+
+      await Singer.updateMany(
+        { albums: req.params.id },
+        { $pull: { albums: req.params.id } }
+      );
+
+      await Album.findByIdAndDelete(req.params.id);
+
+      res.status(200).json("Deleted successfully !");
     } catch (error) {
       res.status(500).json(error);
     }
